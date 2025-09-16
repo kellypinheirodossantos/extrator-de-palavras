97873005502a248d274a84d3a70e9c2340461db8
kellypinheirodossantos <pinheiro.santos.kelly@escola.pr.gov.br>
2025-09-16 16:58:37 -0300

    Add files via upload
    aula do dia 16/09/2025

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function botaoMostraPalavras(){
 
     camporesultado.textContent = palavra.join("")
 }
+function processaTexto (texto){
+    let palavra = texto.split(/\P{L}+/u);
+    const frequncia = contaFrequencia(palavras)
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras)
+
+    function ordenaPalavras(p1,p2){
+        return frequencias [p2]-frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras){
+ let frequencias = {};
+   for (let i of palavras){
+    frequencias[i] = 0;
+     for(let j of palavras){
+         if (i==j) { 
+            frequencias[i]++;
+            }
+        }
+
+    }
+
+}
+
+    return palavras;
+    
+}
